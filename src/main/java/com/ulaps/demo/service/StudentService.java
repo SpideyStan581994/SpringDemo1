5613cc22e1f5c4be6286dd3d8b22b99ed57134bf
SpideyStan581994 <steven.tan@ulaps.dev>
2021-01-26 19:21:14 +0800

    Adding a update function using transactional annotation

--- src/main/java/com/ulaps/demo/service/StudentService.java
+++ src/main/java/com/ulaps/demo/service/StudentService.java
@@ -4,7 +4,10 @@ import com.ulaps.demo.model.Student;
 import com.ulaps.demo.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Component
@@ -38,5 +41,36 @@ public class StudentService {
             throw new IllegalStateException("Student with id " + studentId + " does not exists.");
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId,
+                              String name,
+                              String email) {
+
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(()-> new IllegalStateException(
+                        "student with id " + studentId + " is not exists."));
+
+        if(name != null &&
+                name.length() > 0 &&
+                !Objects.equals(student.getName(),name)) {
+            student.setName(name);
+        }
+
+        if(email != null &&
+                email.length() > 0 &&
+                !Objects.equals(student.getEmail(),email)) {
+
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if(studentOptional.isPresent()){
+                throw new IllegalStateException("email taken");
+            }
+
+            student.setEmail(email);
+        }
+
+
+
     }
 }
