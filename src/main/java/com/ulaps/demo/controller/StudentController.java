07197f9eff4160c5e180b0fc5e8e670ea3651d98
SpideyStan581994 <steven.tan@ulaps.dev>
2021-01-26 17:36:19 +0800

    Create a POST Mapping request.

--- src/main/java/com/ulaps/demo/controller/StudentController.java
+++ src/main/java/com/ulaps/demo/controller/StudentController.java
@@ -3,10 +3,7 @@ package com.ulaps.demo.controller;
 import com.ulaps.demo.model.Student;
 import com.ulaps.demo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -29,8 +26,10 @@ public class StudentController {
        return studentService.getStudents();
     }
 
+
     @PostMapping
-    public void addStudent(){
+    public void registerNewStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
     }
 
 }
--- src/main/java/com/ulaps/demo/service/StudentService.java
+++ src/main/java/com/ulaps/demo/service/StudentService.java
@@ -25,7 +25,7 @@ public class StudentService {
     }
 
 
-    public void addStudent(Student student){
+    public void addNewStudent(Student student){
         System.out.println(student);
     }
 }
