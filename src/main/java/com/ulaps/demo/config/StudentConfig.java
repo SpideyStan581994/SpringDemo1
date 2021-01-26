8b3d142c8fb603d665c429c7d1746153ba3c4609
SpideyStan581994 <steven.tan@ulaps.dev>
2021-01-26 17:14:08 +0800

    Added config class, add a bean to a CommandLineRunner method

---
+++ src/main/java/com/ulaps/demo/config/StudentConfig.java
@@ -0,0 +1,33 @@
+package com.ulaps.demo.config;
+
+import com.ulaps.demo.model.Student;
+import com.ulaps.demo.repositories.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student steven = new Student(1L,
+                    "Steven Tan",
+                    "stan581994@gmail.com",
+                    LocalDate.of(1994, Month.MAY, 8),
+                    26);
+            Student lisa = new Student(2L,
+                    "Lisa Macamundong",
+                    "lisamaca@gmail.com",
+                    LocalDate.of(1998, Month.FEBRUARY, 19),
+                    22);
+
+            studentRepository.saveAll(List.of(steven,lisa));
+        };
+    }
+}
